e3e6c1f7c2d4a94b5fee2795b687a081f2a6e37b
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-02-03 19:17:14 +0300

    There is somthing wrong couldn't find it.

--- lesson-week_04-day_01-rwd/starter-code/js/app.js
+++ lesson-week_04-day_01-rwd/starter-code/js/app.js
@@ -2,5 +2,5 @@ $(function(){
     $('header nav > ul > li:nth-child(4) ul li:first-child').on('click', function(event){
         event.preventDefault();
         $(this).siblings().slideToggle();
-    })
+ })
 });
